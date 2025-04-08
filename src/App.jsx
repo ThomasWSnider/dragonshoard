016b642b868d816e2c50b211d3778656e02e6672
ThomasWSnider <thomas.wayne.snider@gmail.com>
2025-04-08 14:44:28 -0600

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>{count}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increment Count
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Decrement Count
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
