bd7f6012881d72d99a1cce231b7429da2d068a03
ThomasWSnider <thomas.wayne.snider@gmail.com>
2025-04-08 15:23:22 -0600

    added decrement button and par;ity indicator

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,32 @@ import React from "react"
 import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
-
+  const countParity = (count % 2 == 0) ? "even" : "odd";
   return (
-    <>
-      <h1>{count}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increment Count
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          Decrement Count
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 text-center mb-3">
+          <h1>{count}</h1>
+        </div>
+        <div className="col-12 text-center mb-3">
+          <h4>The count is <span className={count % 8 == 0 ? "divisible-by-three" : countParity}>{countParity}</span></h4>
+        </div>
+        <div className="col-6 d-flex justify-content-center pe-3">
+          <button onClick={() => setCount((count) => count - 1)} className="btn btn-danger">
+            Decrement Count
+          </button>
+        </div>
+        <div className="col-6 d-flex justify-content-center ps-3">
+          <button onClick={() => setCount((count) => count + 1)} className="btn btn-success">
+            Increment Count
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
